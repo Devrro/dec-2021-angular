2283b934c7247fd5162ec490fdca2b46fc3f06b1
Serhii_Terletckiy <beinght@gmail.com>
2022-05-17 16:26:09 +0300

    Angular HW1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users/users.component';
+import { UserComponent } from './components/user/user/user.component';
+import { PostsComponent } from './components/posts/posts/posts.component';
+import { PostComponent } from './components/post/post/post.component';
+import { CommentsComponent } from './components/comments/comments/comments.component';
+import { CommentComponent } from './components/comment/comment/comment.component';
+import { HttpClientModule } from "@angular/common/http";
+import {RouterModule} from "@angular/router";
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    PostsComponent,
+    PostComponent,
+    CommentsComponent,
+    CommentComponent,
+    HomePageComponent,
+    UserDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path:'home-page',component: HomePageComponent},
+      {path:'user-page',component: UsersComponent},
+      {path:'user-details/:id',component: UserDetailComponent},
+      ])
   ],
   providers: [],
   bootstrap: [AppComponent]
